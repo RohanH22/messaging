6bbc837e9c13fc75d6f40f81d868ba4ba0da00b5
Rohan <rohan@hh>
2023-04-01 23:01:34 +0530

    gh

--- server.js
+++ server.js
@@ -6,12 +6,20 @@ var server = http.createServer(app);
 var io = require('socket.io')(server)
 // const http = require('http').createServer(app);
 // const io = require('socket.io')(http);
+const cors = require('cors');
+app.use(cors());
+
 
 var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 //DataBase Url 
 var dbUrl = 'mongodb+srv://rohanhunakuppi:HN9T6qcBQqv17veo@learning-node.4vlvfa8.mongodb.net/test'
